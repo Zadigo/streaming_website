7b63e8a1d5599718d9adc4e3fe891a8ead2f9c84
John Pendenque <pendenquejohn@gmail.com>
2020-05-18 11:06:29 +0200

    Updated steaming items

--- mystreaming/streamer/urls.py
+++ mystreaming/streamer/urls.py
@@ -2,8 +2,8 @@ from django.conf.urls import url
 from streamer import views
 
 urlpatterns = [
-    url(r'^start_stream$', views.StartStream.as_view(), name='start_stream'),
-    url(r'^stop_stream$', views.StopStream.as_view(), name='stop_stream'),
+    url(r'^start$', views.StartStream.as_view(), name='start_stream'),
+    url(r'^stop$', views.StopStream.as_view(), name='stop_stream'),
     url(r'^live/(?P<username>\w+)/index.m3u8', views.fake_view, name='current_steam_url'),
     url(r'^$', views.HomePage.as_view(), name='home')
 ]
--- mystreaming/streamer/views.py
+++ mystreaming/streamer/views.py
@@ -1,5 +1,5 @@
 from django import http
-from django.shortcuts import get_object_or_404, render, redirect
+from django.shortcuts import get_object_or_404, render, redirect, reverse
 from django.utils import timezone
 from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import csrf_exempt
@@ -15,31 +15,33 @@ class StartStream(View):
     """ This view is called to start the stream
     """
     def post(self, request, **kwargs):
-        name = request.POST['name']
-        stream = get_object_or_404(models.Stream, key=name)
+        try:
+            key = request.POST['key']
+        except:
+            return http.HttpResponseForbidden()
+        else:
+            stream = get_object_or_404(models.Stream, key=key)
 
         # Ban streamers by setting them inactive
-        # if not stream.user.is_active:
-        #     return HttpResponseForbidden("Inactive user")
+        if not stream.user.is_active:
+            return http.JsonResponse(data={'state': 'forbidden'})
 
         # Don't allow the same stream to be published multiple times
-        # if stream.started_at:
-        #     return HttpResponseForbidden("Already streaming")
+        if stream.started_on:
+            return http.JsonResponse(data={'activity': True})
 
-        stream.started_at = timezone.now()
+        stream.started_on = timezone.now()
         stream.save()
-
-        # Redirect to the streamer's public username
-        return redirect(stream.user.username)
+        return http.JsonResponse(data={'redirect_to': reverse('current_steam_url', args=[stream.user.username])})
 
 @method_decorator(csrf_exempt, 'dispatch')
 class StopStream(View):
     """ This view is called when a stream stops
     """
     def post(self, request, **kwargs):
-        name = request.POST["name"]
-        models.Stream.objects.filter(key=name).update(started_at=None)
-        return http.HttpResponse('OK')
+        key = request.POST['key']
+        models.Stream.objects.filter(key=key).update(started_on=None)
+        return http.JsonResponse(data={'activity': False})
 
 def fake_view(*args, **kwargs):
     """ This view should never be called because the URL paths

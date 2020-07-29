313fa8c962b5978236b63a690145652b7d0f1fa2
root <root@vps775047.ovh.net>
2020-07-29 15:00:05 +0200

    Global changes

--- mystreaming/streamer/urls.py
+++ mystreaming/streamer/urls.py
@@ -4,8 +4,8 @@ from streamer import views
 app_name = 'stream'
 
 urlpatterns = [
-    url(r'^start$', views.StartStream.as_view(), name='start_stream'),
-    url(r'^stop$', views.StopStream.as_view(), name='stop_stream'),
+    url(r'^start$', views.StartStream.as_view(), name='start'),
+    url(r'^stop$', views.StopStream.as_view(), name='stop'),
     url(r'^live/(?P<username>\w+)/index.m3u8', views.fake_view, name='live_stream'),
     url(r'^$', views.HomePage.as_view(), name='home')
 ]

75abb69f7e0e3ce2cfcb956c0c22bd5f129d7dfd
John Pendenque <pendenquejohn@gmail.com>
2020-07-29 13:35:56 +0200

    Global improvements

--- mystreaming/streamer/models.py
+++ mystreaming/streamer/models.py
@@ -2,16 +2,23 @@ import secrets
 
 from django.contrib.auth import get_user_model
 from django.db import models
-from django.shortcuts import reverse
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+from django.shortcuts import reverse
 
-USER = get_user_model()
+MYUSER = get_user_model()
 
 class Stream(models.Model):
-    user    = models.OneToOneField(USER, on_delete=models.CASCADE)
+    user    = models.OneToOneField(MYUSER, on_delete=models.CASCADE)
     key     = models.CharField(max_length=20, default=secrets.token_hex(10), unique=True)
     started_on  = models.DateTimeField(null=True, blank=True)
+
+    objects = models.Manager()
+
+    class Meta:
+        indexes = [
+            models.Index(fields=['key'])
+        ]
 
     def __str__(self):
         return self.key
@@ -24,7 +31,8 @@ class Stream(models.Model):
     def stream_url(self):
         return reverse('current_steam_url', args=[self.user.username])
 
-@receiver(post_save, sender=USER)
+
+@receiver(post_save, sender=MYUSER)
 def create_new_stream(sender, instance, created, **kwargs):
     if created:
         Stream.objects.create(user=instance)
--- mystreaming/streamer/urls.py
+++ mystreaming/streamer/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import url
 from streamer import views
 
+app_name = 'stream'
+
 urlpatterns = [
     url(r'^start$', views.StartStream.as_view(), name='start_stream'),
     url(r'^stop$', views.StopStream.as_view(), name='stop_stream'),
